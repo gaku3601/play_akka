df560d8cddc0c54dc0f50faafc48191c5b5cc765
gaku <pro.gaku@gmail.com>
2020-07-25 10:18:05 +0900

    リファクタ

--- app/actors/Library.scala
+++ app/actors/Library.scala
@@ -1,14 +1,11 @@
 package actors
 
+import actors.Library.{Read, Write}
 import akka.actor.Actor
 import com.google.inject.AbstractModule
 import play.api.libs.concurrent.AkkaGuiceSupport
 
 case class Message(value: String)
-
-case class Read(message: Message)
-
-case class Write(message: Message)
 
 class Library extends Actor {
   override def receive: Receive = {
@@ -20,6 +17,15 @@ class Library extends Actor {
     }
   }
 }
+
+object Library {
+
+  case class Read(message: Message)
+
+  case class Write(message: Message)
+
+}
+
 
 class LibraryClient extends AbstractModule with AkkaGuiceSupport {
   override def configure = bindActor[Library]("library-actor")
